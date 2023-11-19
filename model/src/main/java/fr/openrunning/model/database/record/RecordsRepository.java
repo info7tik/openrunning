21cad55d8cfc5496c832f5356346bbc9b6158413
info7tik <info7tik@gmail.com>
2023-11-19 18:26:20 +0100

    FIX add missing class

--- model/src/main/java/fr/openrunning/model/database/record/RecordsRepository.java
+++ model/src/main/java/fr/openrunning/model/database/record/RecordsRepository.java
@@ -1,5 +1,9 @@
 package fr.openrunning.model.database.record;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +11,18 @@ import fr.openrunning.model.database.TimestampUserPrimaryKey;
 
 @Repository
 public interface RecordsRepository extends CrudRepository<Record, TimestampUserPrimaryKey> {
+    @Query("SELECT DISTINCT r.distanceInMeters FROM Record r")
+    List<Integer> getRecordDistances();
+
+    Optional<Record> findFirstByUserIdAndDistanceInMetersOrderByTimeInSecondsAsc(int userId, int distanceInMeters);
+
+    default Optional<Record> getBestRecordFromDistanceInMeters(int userId, int distanceInMeters) {
+        return this.findFirstByUserIdAndDistanceInMetersOrderByTimeInSecondsAsc(userId, distanceInMeters);
+    }
+
+    List<Record> findByUserIdAndTimestamp(int userId, long timestamp);
+
+    default List<Record> getTrackRecords(int userId, long timestamp) {
+        return this.findByUserIdAndTimestamp(userId, timestamp);
+    }
 }
